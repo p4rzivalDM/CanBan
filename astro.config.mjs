a74a5792e1bf515aebbeb8b2ce02ff013eba84c7
Mirco <mircodian@hotmail.it>
2025-11-25 22:09:36 +0100

    develop - Correzione del percorso di base nella configurazione di Astro
    - Aggiornato il campo 'bsase' in 'astro.config.mjs' per riflettere il percorso corretto.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://p4rzivalDM.github.io',
+  bsase: '/CanBan/',
   integrations: [react()],
 
   vite: {
